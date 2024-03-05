4ec878bc2ab586f76f3bc6e1fab2f20e19eceb1c
Giulio998 <giulio_favia@hotmail.it>
2024-03-05 09:29:25 +0100

    fix

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let km = Number(prompt("Dichiarare la distanza da percorrere"));
 let age = Number(prompt("Inserire l'età del passeggero"));
-const basePrice = (km * 0.21).toFixed(2);
+const basePrice = (km * 0.21);
 let finalPrice;
 let minorPrice = basePrice - (basePrice * 0.2);
 let seniorPrice = basePrice - (basePrice * 0.4);
@@ -15,5 +15,6 @@ if (age < 18) {
     finalPrice = seniorPrice.toFixed(2);
     console.log("seniorPrice", finalPrice);
 } else {
-    console.log("basePrice", basePrice);
+    finalPrice = basePrice.toFixed(2);
+    console.log("basePrice", finalPrice);
 }
